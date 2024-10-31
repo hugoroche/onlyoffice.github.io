95c7c0d8d9b458be22769a4bee8fbf63d5797e33
HugoRoche <hugo.rocha@praxo.ch>
2024-10-31 14:35:53 +0100

    test

--- sdkjs-plugins/content/helloworld/scripts/apertium.js
+++ sdkjs-plugins/content/helloworld/scripts/apertium.js
@@ -84,10 +84,9 @@ function getMessage(key) {
             for (var i = 0; i < aContentControls.length; i++) {
                 if(aContentControls[i].ye.ga.Wa === "${contentControlId}"){
                     console.log("YOUPI 2 ", aContentControls[i]);
-                    aContentControls[i].AddElement(oRun);
+                    aContentControls[i].AddElement(oRun, 0);
                 }
             }
-            oDocument.InsertContent(oRun);
         `;
 
         var _scriptObject = {
@@ -102,7 +101,7 @@ function getMessage(key) {
         };
     
 
-        console.log("44");
+        console.log("45");
         // replace content
         window.Asc.plugin.executeMethod("InsertAndReplaceContentControls", [[_scriptObject], true]);
         /*window.Asc.plugin.executeMethod("AddContentControl", [2, {
