ae8648690a791e4d764b4515ce8185d5b0cf677d
HugoRoche <hugo.rocha@praxo.ch>
2024-10-31 09:42:18 +0100

    test

--- sdkjs-plugins/content/helloworld/code.js
+++ sdkjs-plugins/content/helloworld/code.js
@@ -1,28 +1,5 @@
 // Example set settings to editors
 (function (window, undefined) {
-
-    String.prototype.replaceAll = function (search, replacement) {
-        var target = this;
-        return target.replace(new RegExp(search, 'g'), replacement);
-    };
-
-    function hexToRgb(hex) {
-        // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-        var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-        hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-            return r + r + g + g + b + b;
-        });
-
-        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        return result ? {
-            r: parseInt(result[1], 16),
-            g: parseInt(result[2], 16),
-            b: parseInt(result[3], 16)
-        } : null;
-    }
-
-    var _Control = null;
-
     // plugin init method
     window.Asc.plugin.init = function init() {
         this.callCommand(() => {
